9043ea7dd60a598e5797f8008695852d3b7e9a5d
fujinhua <fu.jinhua@zte.com.cn>
2017-09-06 10:39:04 +0800

    Fix docker build function name
    Change-Id: I7122c0e6805535edce9e733843991c1c0e550ec9
    Issue-Id: VFC-287
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -19,16 +19,16 @@ if [ $HTTPS_PROXY ]; then
     BUILD_ARGS+=" --build-arg HTTPS_PROXY=${HTTPS_PROXY}"
 fi
 
-function build_lcm {
+function build_image {
     echo "Start build docker image: ${IMAGE_NAME}"
     docker build ${BUILD_ARGS} -t ${IMAGE_NAME}:${VERSION} -t ${IMAGE_NAME}:latest .
 }
 
-function push_lcm {
+function push_image {
     echo "Start push docker image: ${IMAGE_NAME}"
     docker push ${IMAGE_NAME}:${VERSION}
     docker push ${IMAGE_NAME}:latest
 }
 
-build_lcm
-push_lcm
+build_image
+push_image
--- docker/run_image.sh
+++ docker/run_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function run_lcm {
-    docker run -it --name vfc-nslcm -p 3306:3306 -p 8403:8403 -e MSB_ADDR=127.0.0.1 onap/vfc/nslcm
+    docker run -it --name vfc-nslcm -p 3306:3306 -p 8403:8403 -e MSB_ADDR=127.0.0.1:80 onap/vfc/nslcm
 }
 
 run_lcm
