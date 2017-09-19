a33e556087b77ed8c916e4b9a3a2f60de92dae82
fujinhua <fu.jinhua@zte.com.cn>
2017-09-19 18:08:04 +0800

    Add call parse csar from catalog
    Change-Id: I4fc17a7f0e1150f131587d23eaa8266ef9edabe2
    Issue-Id: VFC-386
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

---
+++ lcm/pub/msapi/sdc_run_catalog.py
@@ -0,0 +1,38 @@
+# Copyright 2017 ZTE Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import logging
+
+from lcm.pub.utils.restcall import req_by_msb
+from lcm.pub.exceptions import NSLCMException
+
+logger = logging.getLogger(__name__)
+
+
+def parse_nsd(csar_id, input_parameters=[]):
+    req_param = json.JSONEncoder().encode({"csarId": csar_id, "inputs": input_parameters})
+    ret = req_by_msb("/api/catalog/v1/parsernsd", "POST", req_param)
+    if ret[0] != 0:
+        logger.error("Status code is %s, detail is %s.", ret[2], ret[1])
+        raise NSLCMException("Failed to parsernsd of CSAR(%s) from catalog." % csar_id)
+    return json.JSONDecoder().decode(ret[1])
+
+def parse_vnfd(csar_id, input_parameters=[]):
+    req_param = json.JSONEncoder().encode({"csarId": csar_id, "inputs": input_parameters})
+    ret = req_by_msb("/api/catalog/v1/parservnfd", "POST", req_param)
+    if ret[0] != 0:
+        logger.error("Status code is %s, detail is %s.", ret[2], ret[1])
+        raise NSLCMException("Failed to parse_vnfd of CSAR(%s) from catalog." % csar_id)
+    return json.JSONDecoder().decode(ret[1])
