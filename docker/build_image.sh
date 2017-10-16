d695cdf0bae15732beefc78e641924134fcfc898
fujinhua <fu.jinhua@zte.com.cn>
2017-10-16 16:52:25 +0800

    Add vfc docker build staging tag.
    Change-Id: Iff5ea1b71ad5ead1aee89ff523a2ab732688da7b
    Issue-Id: VFC-535
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -6,11 +6,12 @@ cd ${DOCKER_BUILD_DIR}
 
 BUILD_ARGS="--no-cache"
 ORG="onap"
-VERSION="1.0.0-SNAPSHOT"
+VERSION="1.0.0"
 PROJECT="vfc"
 IMAGE="nslcm"
 DOCKER_REPOSITORY="nexus3.onap.org:10003"
 IMAGE_NAME="${DOCKER_REPOSITORY}/${ORG}/${PROJECT}/${IMAGE}"
+TIMESTAMP=$(date +"%Y%m%dT%H%M%S")
 
 if [ $HTTP_PROXY ]; then
     BUILD_ARGS+=" --build-arg HTTP_PROXY=${HTTP_PROXY}"
@@ -21,13 +22,23 @@ fi
 
 function build_image {
     echo "Start build docker image: ${IMAGE_NAME}"
-    docker build ${BUILD_ARGS} -t ${IMAGE_NAME}:${VERSION} -t ${IMAGE_NAME}:latest .
+    docker build ${BUILD_ARGS} -t ${IMAGE_NAME}:latest .
+}
+
+function push_image_tag {
+    TAG_NAME=$1
+    echo "Start push ${TAG_NAME}"
+    docker tag ${IMAGE_NAME}:latest ${TAG_NAME}
+    docker push ${TAG_NAME}
 }
 
 function push_image {
-    echo "Start push docker image: ${IMAGE_NAME}"
-    docker push ${IMAGE_NAME}:${VERSION}
+    echo "Start push ${IMAGE_NAME}:latest"
     docker push ${IMAGE_NAME}:latest
+    
+    push_image_tag ${IMAGE_NAME}:${VERSION}-SNAPSHOT-latest
+    push_image_tag ${IMAGE_NAME}:${VERSION}-STAGING-latest
+    push_image_tag ${IMAGE_NAME}:${VERSION}-STAGING-${TIMESTAMP}
 }
 
 build_image
