1560772d1a8648bf8f6313869a35ec45639c22d9
fujinhua <fu.jinhua@zte.com.cn>
2017-10-26 11:36:19 +0800

    Fix bugs for integeration in cmcc lab
    Change return value for nsd/vnfd convert
    
    Change-Id: Iad062da1d53e01ddc05a1e100d7fc2db85181d14
    Issue-Id: VFC-558
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- lcm/pub/msapi/sdc_run_catalog.py
+++ lcm/pub/msapi/sdc_run_catalog.py
@@ -27,7 +27,7 @@ def parse_nsd(csar_id, input_parameters=[]):
     if ret[0] != 0:
         logger.error("Status code is %s, detail is %s.", ret[2], ret[1])
         raise NSLCMException("Failed to parsernsd of CSAR(%s) from catalog." % csar_id)
-    return json.JSONDecoder().decode(ret[1])
+    return ret[1]
 
 
 def parse_vnfd(csar_id, input_parameters=[]):
@@ -36,8 +36,7 @@ def parse_vnfd(csar_id, input_parameters=[]):
     if ret[0] != 0:
         logger.error("Status code is %s, detail is %s.", ret[2], ret[1])
         raise NSLCMException("Failed to parse_vnfd of CSAR(%s) from catalog." % csar_id)
-    return json.JSONDecoder().decode(ret[1])
-
+    return ret[1]
 
 def query_nspackage_by_id(csar_id):
     ret = req_by_msb("/api/catalog/v1/nspackages/%s" % csar_id, "GET")
