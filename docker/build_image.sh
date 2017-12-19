99b165a8bb3872d63c27aa5f681ac18af6654777
fujinhua <fu.jinhua@zte.com.cn>
2017-12-19 15:39:48 +0800

    Update VFC version number
    Change-Id: I8958191d3aa92bc35259ad80eb4360f5fccfb7c7
    Issue-ID: VFC-624
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -6,7 +6,7 @@ cd ${DOCKER_BUILD_DIR}
 
 BUILD_ARGS="--no-cache"
 ORG="onap"
-VERSION="1.0.2"
+VERSION="1.0.3"
 PROJECT="vfc"
 IMAGE="nslcm"
 DOCKER_REPOSITORY="nexus3.onap.org:10003"
@@ -21,20 +21,20 @@ if [ $HTTPS_PROXY ]; then
 fi
 
 function build_image {
-    echo "Start build docker image: ${IMAGE_NAME}"
-    docker build ${BUILD_ARGS} -t ${IMAGE_NAME}:latest .
+    echo "Start build docker image: ${IMAGE_NAME}:${VERSION}-latest"
+    docker build ${BUILD_ARGS} -t ${IMAGE_NAME}:${VERSION}-latest .
 }
 
 function push_image_tag {
     TAG_NAME=$1
     echo "Start push ${TAG_NAME}"
-    docker tag ${IMAGE_NAME}:latest ${TAG_NAME}
+    docker tag ${IMAGE_NAME}:${VERSION}-latest ${TAG_NAME}
     docker push ${TAG_NAME}
 }
 
 function push_image {
-    echo "Start push ${IMAGE_NAME}:latest"
-    docker push ${IMAGE_NAME}:latest
+    echo "Start push ${IMAGE_NAME}:${VERSION}-latest"
+    docker push ${IMAGE_NAME}:${VERSION}-latest
     
     push_image_tag ${IMAGE_NAME}:${VERSION}-SNAPSHOT-latest
     push_image_tag ${IMAGE_NAME}:${VERSION}-STAGING-latest
