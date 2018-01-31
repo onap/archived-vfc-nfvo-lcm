1362afd5acc7d771cc4912c6d38d6d42fd520441
fujinhua <fu.jinhua@zte.com.cn>
2018-01-31 13:27:10 +0800

    Add create sfc serializer
    Change-Id: I4b8a1ad076b69908bff0af26c36a965165c09f52
    Issue-ID: VFC-694
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- lcm/ns/sfcs/serializers.py
+++ lcm/ns/sfcs/serializers.py
@@ -24,3 +24,15 @@ class CreateSfcInstReqSerializer(serializers.Serializer):
 
 class CreateSfcInstRespSerializer(serializers.Serializer):
     fpinstid = serializers.CharField(help_text="ID of FP instance", required=True)
+
+
+class CreateSfcReqSerializer(serializers.Serializer):
+    fpindex = serializers.CharField(help_text="Index of FP", required=True)
+    nsInstanceId = serializers.CharField(help_text="ID of NS instance", required=False, allow_null=True)
+    context = serializers.CharField(help_text="Context of NS instance", required=False, allow_null=True)
+    sdnControllerId = serializers.CharField(help_text="ID of SDN controller", required=False, allow_null=True)
+
+
+class CreateSfcRespSerializer(serializers.Serializer):
+    jobId = serializers.CharField(help_text="ID of job", required=True)
+    sfcInstId = serializers.CharField(help_text="ID of SFC instance", required=True)
