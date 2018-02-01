4ed01d6a8f8f46f6b419e737364deb2b28aea4f9
fujinhua <fu.jinhua@zte.com.cn>
2018-02-01 15:13:13 +0800

    Add inst vnf req serializer
    Change-Id: If36c8e308839bb1969de0e614f258b620b1c60c9
    Issue-ID: VFC-714
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

---
+++ lcm/ns/vnfs/serializers.py
@@ -0,0 +1,21 @@
+# Copyright 2018 ZTE Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from rest_framework import serializers
+
+
+class InstVnfReqSerializer(serializers.Serializer):
+    vnfIndex = serializers.CharField(help_text="Index of VNF", required=True)
+    nsInstanceId = serializers.CharField(help_text="ID of NS instance", required=True)
+    additionalParamForVnf = serializers.CharField(help_text="Additional param for VNF", required=False, allow_null=True)
