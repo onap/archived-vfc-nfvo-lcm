a87774d832726ae0e64f377786c61dd197c12a72
fujinhua <fu.jinhua@zte.com.cn>
2018-02-26 16:43:05 +0800

    Add UT cases for sfc instance
    Change-Id: Id7ac5d682d5c7b647a16219c79f161aed8ef6f6a
    Issue-ID: VFC-754
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- lcm/ns/tests/sfcs/test_sfc_instance.py
+++ lcm/ns/tests/sfcs/test_sfc_instance.py
@@ -1,4 +1,4 @@
-# Copyright 2016 ZTE Corporation.
+# Copyright 2018 ZTE Corporation.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,37 +11,37 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# import json
-# from rest_framework import status
-# from test_data import nsd_model
-# from django.test import Client
-# from django.test import TestCase
-# from lcm.pub.database.models import FPInstModel, VNFFGInstModel
-#
-#
-# class TestSfc(TestCase):
-#     def setUp(self):
-#         self.client = Client()
-#         VNFFGInstModel.objects.all().delete()
-#         FPInstModel.objects.all().delete()
-#         VNFFGInstModel(vnffgdid="vnffg_id1", vnffginstid="vnffg_inst_1", nsinstid="ns_inst_1", endpointnumber=2,
-#                        vllist="vlinst1", cplist="cp1", vnflist="vnf1,vnf2").save()
-#
-#     def tearDown(self):
-#         VNFFGInstModel.objects.all().delete()
-#         FPInstModel.objects.all().delete()
-#
-#     def test_sfc_instance_success(self):
-#         data = {
-#             "nsinstanceid": "ns_inst_1",
-#             "context": json.dumps(nsd_model),
-#             "fpindex": "fpd_1",
-#             "sdncontrollerid": "sdnControllerId_1"
-#         }
-#         resp = self.client.post("/api/nslcm/v1/ns/sfc_instance", data, format='json')
-#
-#         vnffg = VNFFGInstModel.objects.get(vnffginstid="vnffg_inst_1")
-#         ret = FPInstModel.objects.get(fpinstid=resp.data["fpinstid"])
-#         self.assertEqual(resp.status_code, status.HTTP_200_OK)
-#         self.assertEqual(vnffg.fplist, resp.data["fpinstid"])
-#         self.assertIsNotNone(ret)
+import json
+from rest_framework import status
+from test_data import nsd_model
+from django.test import Client
+from django.test import TestCase
+from lcm.pub.database.models import FPInstModel, VNFFGInstModel
+
+
+class TestSfc(TestCase):
+    def setUp(self):
+        self.client = Client()
+        VNFFGInstModel.objects.all().delete()
+        FPInstModel.objects.all().delete()
+        VNFFGInstModel(vnffgdid="vnffg_id1", vnffginstid="vnffg_inst_1", nsinstid="ns_inst_1", endpointnumber=2,
+                       vllist="vlinst1", cplist="cp1", vnflist="vnf1,vnf2").save()
+
+    def tearDown(self):
+        VNFFGInstModel.objects.all().delete()
+        FPInstModel.objects.all().delete()
+
+    def test_sfc_instance_success(self):
+        data = {
+            "nsInstanceId": "ns_inst_1",
+            "context": json.dumps(nsd_model),
+            "fpindex": "fpd_1",
+            "sdnControllerId": "sdnControllerId_1"
+        }
+        resp = self.client.post("/api/nslcm/v1/ns/sfc_instance", data, format='json')
+
+        self.assertEqual(resp.status_code, status.HTTP_200_OK, resp.data)
+        vnffg = VNFFGInstModel.objects.get(vnffginstid="vnffg_inst_1")
+        ret = FPInstModel.objects.get(fpinstid=resp.data["fpinstid"])
+        self.assertEqual(vnffg.fplist, resp.data["fpinstid"])
+        self.assertIsNotNone(ret)
