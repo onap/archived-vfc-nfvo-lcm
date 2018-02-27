c82a58b355109884c4dbbc9d63fff96670b60046
fujinhua <fu.jinhua@zte.com.cn>
2018-02-27 16:26:02 +0800

    Add VNF instance serializer
    Change-Id: I55365adf70ddc9fc47c0c39477661f96b2889665
    Issue-ID: VFC-779
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- lcm/v2/serializers.py
+++ lcm/v2/serializers.py
@@ -16,6 +16,24 @@ from rest_framework import serializers
 
 
 class CreateVnfRequestSerializer(serializers.Serializer):
-    vnfdId = serializers.CharField(help_text="Identifier that identifies the VNFD which defines the VNF instance to be created", required=True)
-    vnfInstanceName = serializers.CharField(help_text="Human-readable name of the VNF instance to be created", required=False, allow_null=True, allow_blank=True)
-    vnfInstanceDescription = serializers.CharField(help_text="Human-readable description of the VNF instance to be created", required=False, allow_null=True, allow_blank=True)
+    vnfdId = serializers.CharField(help_text="Identifier that identifies the VNFD which defines the VNF instance to be created.", required=True)
+    vnfInstanceName = serializers.CharField(help_text="Human-readable name of the VNF instance to be created.", required=False, allow_null=True, allow_blank=True)
+    vnfInstanceDescription = serializers.CharField(help_text="Human-readable description of the VNF instance to be created.", required=False, allow_null=True, allow_blank=True)
+
+
+class VnfInstanceSerializer(serializers.Serializer):
+    id = serializers.CharField(help_text="Identifier of the VNF instance", required=True)
+    vnfInstanceName = serializers.CharField(help_text="Name of the VNF instance.", required=False, allow_null=True, allow_blank=True)
+    vnfInstanceDescription = serializers.CharField(help_text="Human-readable description of the VNF instance.", required=False, allow_null=True, allow_blank=True)
+    vnfdId = serializers.CharField(help_text="Identifier of the VNFD on which the VNF instance is based.", required=False, allow_null=True, allow_blank=True)
+    vnfProvider = serializers.CharField(help_text="Provider of the VNF and the VNFD. The value is copied from the VNFD.", required=False, allow_null=True, allow_blank=True)
+    vnfProductName = serializers.CharField(help_text="Name to identify the VNF Product. The value is copied from the VNFD.", required=False, allow_null=True, allow_blank=True)
+    vnfSoftwareVersion = serializers.CharField(help_text="Software version of the VNF. The value is copied from the VNFD.", required=False, allow_null=True, allow_blank=True)
+    vnfdVersion = serializers.CharField(help_text="Identifies the version of the VNFD. The value is copied from the VNFD.", required=False, allow_null=True, allow_blank=True)
+    vnfPkgId = serializers.CharField(help_text="Identifier of information held by the NFVO about the specific VNF package on which the VNF is based.", required=False, allow_null=True, allow_blank=True)
+    vnfConfigurableProperties = serializers.DictField(
+        help_text="Current values of the configurable properties of the VNF instance.",
+        child=serializers.CharField(help_text="Vnf Configurable Properties", allow_blank=True),
+        required=False,
+        allow_null=True
+    )
