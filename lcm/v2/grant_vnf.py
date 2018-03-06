98283c4dcd3a175d0895fa429b090b1143d0e221
fujinhua <fu.jinhua@zte.com.cn>
2018-03-06 16:35:02 +0800

    Add vnf Grant bussiness logic
    Change-Id: I15acd9944ff4aa3fb55d189bde70b65302814f2a
    Issue-ID: VFC-779
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

---
+++ lcm/v2/grant_vnf.py
@@ -0,0 +1,48 @@
+# Copyright 2018 ZTE Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import logging
+import uuid
+
+from lcm.pub.exceptions import NSLCMException
+
+logger = logging.getLogger(__name__)
+
+
+class GrantVnf(object):
+    def __init__(self, grant_data):
+        self.data = grant_data
+
+    def exec_grant(self):
+        if isinstance(self.data, (unicode, str)):
+            self.data = json.JSONDecoder().decode(self.data)
+        grant_resp = {
+            "id": str(uuid.uuid4()),
+            "vnfInstanceId": self.data.get("vnfInstanceId"),
+            "vnfLcmOpOccId": self.data.get("vnfLcmOpOccId"),
+            "_links": {
+                "self": {
+                    "href": "/grants"
+                },
+                "vnfLcmOpOcc": {
+                    "href": self.data.get("_links").get("vnfLcmOpOcc")
+                },
+                "vnfInstance": {
+                    "href": self.data.get("_links").get("vnfInstance")
+                }
+            }
+        }
+        logger.debug("grant_resp=%s", grant_resp)
+        return grant_resp
