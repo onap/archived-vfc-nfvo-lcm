c2ed095baadb1e6f95055d0ab95138d017824efe
ying.yunlong <ying.yunlong@zte.com.cn>
2018-04-13 11:09:53 +0800

    Change vfc-nslcm swagger url directory
    Change-Id: Idb5ae00a7a5f480f3e494d029a94452f88c3c221
    Issue-ID: VFC-878
    Signed-off-by: ying.yunlong <ying.yunlong@zte.com.cn>

--- lcm/swagger/urls.py
+++ lcm/swagger/urls.py
@@ -12,16 +12,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from django.conf.urls import url
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
-from lcm.swagger.views import SchemaView
 from lcm.swagger.views import SwaggerJsonView
+
+swagger_info = openapi.Info(
+    title="vfc-nfvo-lcm API",
+    default_version='v1',
+    description="""
+
+The `swagger-ui` view can be found [here](/cached/swagger).
+The `ReDoc` view can be found [here](/cached/redoc).
+The swagger YAML document can be found [here](/cached/swagger.yaml)."""
+)
+
+SchemaView = get_schema_view(
+    validators=['ssv', 'flex'],
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     url(r'^api/nslcm/v1/swagger.json$', SwaggerJsonView.as_view()),
     url(r'^swagger(?P<format>.json|.yaml)$', SchemaView.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', SchemaView.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', SchemaView.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    url(r'^cached/swagger(?P<format>.json|.yaml)$', SchemaView.without_ui(cache_timeout=None), name='cschema-json'),
-    url(r'^cached/swagger/$', SchemaView.with_ui('swagger', cache_timeout=None), name='cschema-swagger-ui'),
-    url(r'^cached/redoc/$', SchemaView.with_ui('redoc', cache_timeout=None), name='cschema-redoc'),
 ]
