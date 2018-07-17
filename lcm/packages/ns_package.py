9570d0918585592a3e4cc2d7fb2a94bfcc9439b0
bharath <bharath.thiruveedula@verizon.com>
2018-07-17 09:09:48 +0530

    Remove unused endpoints of nspackages
    Change-Id: Id26792384dfe55940d45818da5d6d6fd161040ef
    Issue-ID: VFC-958
    Signed-Off-By: Bharath Thiruveedula <bharath.thiruveedula@verizon.com>

--- lcm/packages/ns_package.py
+++ lcm/packages/ns_package.py
@@ -12,19 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
 import logging
 
 import traceback
 import sys
 
-from lcm.pub.database.models import NSDModel, NSInstModel, NfPackageModel
+from lcm.pub.database.models import NSDModel, NSInstModel
 from lcm.pub.utils.values import ignore_case_get
-from lcm.pub.msapi.catalog import STATUS_ONBOARDED, P_STATUS_ENABLED
 from lcm.pub.msapi.catalog import query_csar_from_catalog, set_csar_state
-from lcm.pub.msapi.catalog import query_rawdata_from_catalog, delete_csar_from_catalog
+from lcm.pub.msapi.catalog import delete_csar_from_catalog
 from lcm.pub.exceptions import NSLCMException
-from lcm.pub.utils import toscautil
 
 logger = logging.getLogger(__name__)
 
@@ -53,32 +50,12 @@ def ns_common_call(fun, csar_id, operation=""):
     return fmt_ns_pkg_rsp(STATUS_SUCCESS, ret[1], "")
 
 
-def ns_on_boarding(csar_id):
-    return ns_common_call(NsPackage().on_boarding, csar_id)
-
-
-def ns_delete_csar(csar_id):
-    return ns_common_call(NsPackage().delete_csar, csar_id)
-
-
 def ns_delete_pending_csar(csar_id):
     return ns_common_call(NsPackage().delete_pending_csar, csar_id)
 
 
 def ns_set_state_csar(csar_id, operation):
     return ns_common_call(NsPackage().set_state_csar, csar_id, operation)
-
-
-def ns_get_csar(csar_id):
-    ret = None
-    try:
-        ret = NsPackage().get_csar(csar_id)
-    except NSLCMException as e:
-        return [1, e.message]
-    except:
-        logger.error(traceback.format_exc())
-        return [1, str(sys.exc_info())]
-    return ret
 
 
 ###############################################################################################################
@@ -91,41 +68,6 @@ class NsPackage(object):
 
     def __init__(self):
         pass
-
-    def on_boarding(self, csar_id):
-        csar = query_csar_from_catalog(csar_id)
-        if ignore_case_get(csar, "onBoardState") == STATUS_ONBOARDED:
-            raise NSLCMException("CSAR(%s) already onBoarded." % csar_id)
-
-        raw_data = query_rawdata_from_catalog(csar_id)
-        nsd = toscautil.convert_nsd_model(raw_data["rawData"])  # convert to inner json
-        nsd = json.JSONDecoder().decode(nsd)
-        nsd_id = nsd["metadata"]["id"]
-        if NSDModel.objects.filter(nsd_id=nsd_id):
-            raise NSLCMException("NSD(%s) already exists." % nsd_id)
-
-        for vnf in nsd["vnfs"]:
-            vnfd_id = vnf["properties"]["id"]
-            pkg = NfPackageModel.objects.filter(vnfdid=vnfd_id)
-            if not pkg:
-                raise NSLCMException("VNF package(%s) is not onBoarded." % vnfd_id)
-            pkg_id = pkg[0].nfpackageid
-            if query_csar_from_catalog(pkg_id, "onBoardState") != STATUS_ONBOARDED:
-                raise NSLCMException("VNF package(%s) is not onBoarded on catalog." % pkg_id)
-
-        NSDModel(
-            id=csar_id,
-            nsd_id=nsd_id,
-            name=nsd["metadata"].get("name", nsd_id),
-            vendor=nsd["metadata"].get("vendor", "undefined"),
-            description=nsd["metadata"].get("description", ""),
-            version=nsd["metadata"].get("version", "undefined"),
-            nsd_model=json.JSONEncoder().encode(nsd)).save()
-
-        set_csar_state(csar_id, "onBoardState", STATUS_ONBOARDED)
-        set_csar_state(csar_id, "operationalState", P_STATUS_ENABLED)
-
-        return [0, "CSAR(%s) onBoarded successfully." % csar_id]
 
     def delete_csar(self, csar_id):
         if not NSDModel.objects.filter(id=csar_id):
@@ -155,28 +97,6 @@ class NsPackage(object):
         if ret[0] == 0:
             NSDModel.objects.filter(id=csar_id).delete()
         return ret
-
-    def get_csar(self, csar_id):
-        package_info = {}
-
-        csar_in_catalog = query_csar_from_catalog(csar_id)
-        props_of_catalog = [
-            "name", "provider", "version", "operationalState", "usageState",
-            "onBoardState", "processState", "deletionPending", "downloadUri",
-            "createTime", "modifyTime", "format", "size"]
-        for prop in props_of_catalog:
-            package_info[prop] = ignore_case_get(csar_in_catalog, prop)
-
-        csars = NSDModel.objects.filter(id=csar_id)
-        if csars:
-            package_info["nsdId"] = csars[0].nsd_id
-            package_info["nsdProvider"] = csars[0].vendor
-            package_info["nsdVersion"] = csars[0].version
-
-        nss = NSInstModel.objects.filter(nspackage_id=csar_id)
-        ns_instance_info = [{"nsInstanceId": ns.id, "nsInstanceName": ns.name} for ns in nss]
-
-        return [0, {"csarId": csar_id, "packageInfo": package_info, "nsInstanceInfo": ns_instance_info}]
 
     def set_state_csar(self, csar_id, operation):
         if not NSDModel.objects.filter(id=csar_id):
