064db2858005b9691a569dbf554006f48b3883fb
fujinhua <fu.jinhua@zte.com.cn>
2018-08-07 15:55:18 +0800

    Remove DB and redis service from nslcm
    Change-Id: Iebc919306d93cfac848a03fee2aff23c0b287601
    Issue-ID: VFC-977
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- docker/instance_init.sh
+++ docker/instance_init.sh
@@ -1,23 +1,2 @@
 #!/bin/bash
-
-function start_redis_server {
-    redis-server &
-}
-
-function start_mysql {
-    service mysql start
-    sleep 1
-}
-
-function create_database {
-    cd /service/vfc/nfvo/lcm/resources/bin
-    if [ ! -f dbexist.txt ]; then
-        echo 1 > dbexist.txt
-        bash initDB.sh root $MYSQL_ROOT_PASSWORD 3306 127.0.0.1
-    fi
-    cd /service
-}
-
-start_redis_server
-start_mysql
-create_database
+echo "No service needs init."
