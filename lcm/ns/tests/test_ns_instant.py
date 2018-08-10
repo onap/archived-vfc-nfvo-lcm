41c1c51dd144d03bb12553689f7f78ea6b7f6517
fujinhua <fu.jinhua@zte.com.cn>
2018-08-10 14:08:08 +0800

    Add unit test for ns instantiate
    Change-Id: I9423cd90fed4ea9f73a4c7e4225e7b55c0fff2ef
    Issue-ID: VFC-1009
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- lcm/ns/tests/test_ns_instant.py
+++ lcm/ns/tests/test_ns_instant.py
@@ -19,27 +19,35 @@ import mock
 
 from lcm.pub.database.models import NSInstModel
 from lcm.ns.ns_instant import InstantNSService
+from lcm.pub.utils import restcall
 
 
 class TestNsInstant(TestCase):
     def setUp(self):
         self.client = Client()
         NSInstModel.objects.filter().delete()
-        self.ns_inst_id = "2"
+        self.url = "/api/nslcm/v1/ns/2/instantiate"
+        self.req_data = {
+            "additionalParamForNs": {
+                "location": "1",
+                "sdnControllerId": "2"
+            }
+        }
         NSInstModel(id="2", nspackage_id="7", nsd_id="2").save()
 
     def tearDown(self):
         pass
 
     @mock.patch.object(InstantNSService, 'do_biz')
-    def testNsInstantNormal(self, mock_do_biz):
+    def test_ns_instantiate_normal(self, mock_do_biz):
         mock_do_biz.return_value = dict(data={'jobId': "1"}, status=status.HTTP_200_OK)
-        data = {
-            "additionalParamForNs": {
-                "location": "1",
-                "sdnControllerId": "2"
-            }
-        }
-        resp = self.client.post("/api/nslcm/v1/ns/%s/instantiate" % self.ns_inst_id, data=data)
+        resp = self.client.post(self.url, data=self.req_data)
         self.failUnlessEqual(status.HTTP_200_OK, resp.status_code)
         self.assertEqual({'jobId': "1"}, resp.data)
+
+    @mock.patch.object(restcall, 'call_req')
+    def test_ns_instantiate_when_fail_to_parse_nsd(self, mock_call_req):
+        mock_call_req.return_value = [1, "Failed to parse nsd", '500']
+        resp = self.client.post(self.url, data=self.req_data)
+        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+        self.assertIn("error", response.data)
