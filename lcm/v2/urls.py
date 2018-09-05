2976b5a94d549881de3bedfd316d28cdda1b6369
laili <lai.li@zte.com.cn>
2018-09-05 16:44:05 +0800

    Notification endpoint stuffs.
    Add two notification serializers.
    
    Change-Id: I1c1ed3a7851580cc94a7a56b4ff0cad5936f94a7
    Issue-ID: VFC-1095
    Signed-off-by: laili <lai.li@zte.com.cn>

--- lcm/v2/urls.py
+++ lcm/v2/urls.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 
