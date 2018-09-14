e806a17cbac7e6d2e8188d31004025d4771c9f12
tianxing <15210838572@139.com>
2018-09-14 15:41:51 +0800

    add function of update
    Change-Id: Ie0e6d3b91b9f542399259691969076ea56978e20
    Issue-ID: VFC-1122
    Signed-off-by: tianxing <15210838572@139.com>

--- lcm/ns/biz/ns_update.py
+++ lcm/ns/biz/ns_update.py
@@ -11,19 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# Copyright 2017 ZTE Corporation.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 import logging
 import threading
 import traceback
@@ -42,7 +29,6 @@ logger = logging.getLogger(__name__)
 
 
 class NSUpdateService(threading.Thread):
-
     def __init__(self, ns_instance_id, request_data, job_id):
         super(NSUpdateService, self).__init__()
         self.ns_instance_id = ns_instance_id
@@ -73,28 +59,20 @@ class NSUpdateService(threading.Thread):
         ns_info = NSInstModel.objects.filter(id=self.ns_instance_id)
         if not ns_info:
             logger.error('NS [id=%s] does not exist' % self.ns_instance_id)
-            raise NSLCMException(
-                'NS [id=%s] does not exist' %
-                self.ns_instance_id)
+            raise NSLCMException('NS [id=%s] does not exist' % self.ns_instance_id)
 
         self.update_type = ignore_case_get(self.request_data, "updateType")
         if not self.update_type:
-            logger.error(
-                'UpdateType parameter does not exist or value is incorrect.')
-            raise NSLCMException(
-                'UpdateType parameter does not exist or value incorrect.')
+            logger.error('UpdateType parameter does not exist or value is incorrect.')
+            raise NSLCMException('UpdateType parameter does not exist or value incorrect.')
 
     def do_update(self):
         if self.update_type == "OPERATE_VNF":
-            self.operate_vnf_data = ignore_case_get(
-                self.request_data, "operateVnfData")
-            ns_update_params = self.prepare_update_params(
-                self.operate_vnf_data)
+            self.operate_vnf_data = ignore_case_get(self.request_data, "operateVnfData")
+            ns_update_params = self.prepare_update_params(self.operate_vnf_data)
             status = self.do_ns_update(ns_update_params, 15)
             if status is JOB_MODEL_STATUS.FINISHED:
-                logger.info(
-                    'nf[%s] update handle end' %
-                    ns_update_params.get('nsInstanceId'))
+                logger.info('nf[%s] update handle end' % ns_update_params.get('nsInstanceId'))
                 self.update_job(90,
                                 desc='nf[%s] update handle end' % ns_update_params.get('nsInstanceId'))
             else:
@@ -108,10 +86,7 @@ class NSUpdateService(threading.Thread):
         ns_instance_id = ns_update_params.get('nsInstanceId')
         nf_service = NFUpdateService(ns_instance_id, ns_update_params)
         nf_service.start()
-        self.update_job(
-            progress,
-            desc='nf[%s] update handle start' %
-            ns_instance_id)
+        self.update_job(progress, desc='nf[%s] update handle start' % ns_instance_id)
         status = self.wait_job_finish(nf_service.job_id)
         return status
 
@@ -131,18 +106,15 @@ class NSUpdateService(threading.Thread):
 
         stop_type = ''
         graceful_stop_timeout = ''
-        operational_states = ignore_case_get(
-            change_state_to, 'OperationalStates')
+        operational_states = ignore_case_get(change_state_to, 'OperationalStates')
         if operational_states == "STARTED":
             pass
         elif operational_states == "STOPPED":
             stop_type = ignore_case_get(vnf_data, 'stopType')
             if stop_type == "GRACEFUL":
-                graceful_stop_timeout = ignore_case_get(
-                    vnf_data, 'gracefulStopTimeout')
+                graceful_stop_timeout = ignore_case_get(vnf_data, 'gracefulStopTimeout')
                 if not graceful_stop_timeout:
-                    logger.error(
-                        'gracefulStopTimeout does not exist or value is incorrect.')
+                    logger.error('gracefulStopTimeout does not exist or value is incorrect.')
                     raise NSLCMException(
                         'gracefulStopTimeout does not exist or value is incorrect.')
             elif stop_type == "FORCEFUL":
@@ -152,8 +124,7 @@ class NSUpdateService(threading.Thread):
                 raise NSLCMException('StopType should be GRACEFUL or FORCEFUL')
         else:
             logger.error('OperationalStates should be started or stopped')
-            raise NSLCMException(
-                'OperationalStates should be started or stopped')
+            raise NSLCMException('OperationalStates should be started or stopped')
 
         result = {
             "nsInstanceId": ns_instance_id,
@@ -185,6 +156,4 @@ class NSUpdateService(threading.Thread):
         JobUtil.add_job_status(self.job_id, progress, desc)
 
     def update_ns_status(self, status):
-        NSInstModel.objects.filter(
-            id=self.ns_instance_id).update(
-                status=status)
+        NSInstModel.objects.filter(id=self.ns_instance_id).update(status=status)
