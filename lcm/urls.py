ef7101bfa445d92e762011bfb49c3d72f73113e8
tianxing <15210838572@139.com>
2018-09-14 10:23:51 +0800

    add url
    Change-Id: I04ffbb51a0e2f1434ff83047f4531d422181bb60
    Issue-ID: VFC-1119
    Signed-off-by: tianxing <15210838572@139.com>

--- lcm/urls.py
+++ lcm/urls.py
@@ -1,4 +1,4 @@
-# Copyright 2016 ZTE Corporation.
+# Copyright 2016-2017 ZTE Corporation.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,31 +11,26 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 
-from django.conf.urls import include, url
-
-from lcm.pub.config.config import DEPLOY_WORKFLOW_WHEN_START
-from lcm.pub.config.config import REG_TO_MSB_WHEN_START, REG_TO_MSB_REG_URL, REG_TO_MSB_REG_PARAM
-
+from lcm.ns.views.views import CreateNSView, NSInstView, TerminateNSView, NSDetailView, NSInstPostDealView, \
+    NSManualScaleView, NSHealView, NSUpdateView
 
 urlpatterns = [
-    url(r'^', include('lcm.samples.urls')),
-    url(r'^', include('lcm.ns_vnfs.urls')),
-    url(r'^', include('lcm.ns_vls.urls')),
-    url(r'^', include('lcm.ns_sfcs.urls')),
-    url(r'^', include('lcm.ns.urls')),
-    url(r'^', include('lcm.jobs.urls')),
-    url(r'^', include('lcm.workflows.urls')),
-    url(r'^', include('lcm.swagger.urls')),
+    # API will be deprecated in the future release
+
+    url(r'^api/nslcm/v1/ns$', CreateNSView.as_view()),
+    url(r'^api/nslcm/v1/ns/(?P<ns_instance_id>[0-9a-zA-Z_-]+)/instantiate$', NSInstView.as_view()),
+    url(r'^api/nslcm/v1/ns/(?P<ns_instance_id>[0-9a-zA-Z_-]+)/terminate$', TerminateNSView.as_view()),
+    url(r'^api/nslcm/v1/ns/(?P<ns_instance_id>[0-9a-zA-Z_-]+)$', NSDetailView.as_view()),
+    url(r'^api/nslcm/v1/ns/(?P<ns_instance_id>[0-9a-zA-Z_-]+)/postdeal$', NSInstPostDealView.as_view()),
+    url(r'^api/nslcm/v1/ns/(?P<ns_instance_id>[0-9a-zA-Z_-]+)/scale$', NSManualScaleView.as_view()),
+    url(r'^api/nslcm/v1/ns/(?P<ns_instance_id>[0-9a-zA-Z_-]+)/heal$', NSHealView.as_view()),
+
+    # SOL005 URL API definition TODO
+    url(r'^api/nslcm/v1/ns/(?P<ns_instance_id>[0-9a-zA-Z_-]+)/update$', NSUpdateView.as_view())
+
 ]
 
-# regist to MSB when startup
-if REG_TO_MSB_WHEN_START:
-    import json
-    from lcm.pub.utils.restcall import req_by_msb
-    req_by_msb(REG_TO_MSB_REG_URL, "POST", json.JSONEncoder().encode(REG_TO_MSB_REG_PARAM))
-
-# deploy workflow when startup
-if DEPLOY_WORKFLOW_WHEN_START:
-    from lcm.workflows import auto_deploy
-    auto_deploy.deploy_workflow_on_startup()
+urlpatterns = format_suffix_patterns(urlpatterns)
