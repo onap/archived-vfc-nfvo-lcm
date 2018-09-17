98ed219564d3d6293ecc802e994d3b5709d13666
maopengzhang <zhang.maopeng1@zte.com.cn>
2018-09-17 16:08:32 +0800

    Support PNF In NSLCM
    Support PNF in NSLCM
    
    Change-Id: I51b4e78d24c1fd04d2700fe79a2f6f14a9a69ac8
    Issue-ID: VFC-1102
    Signed-off-by: maopengzhang <zhang.maopeng1@zte.com.cn>

---
+++ lcm/ns_pnfs/tests/test_create_pnf.py
@@ -0,0 +1,90 @@
+# Copyright 2016 ZTE Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import uuid
+import mock
+import json
+from django.test import TestCase, Client
+from rest_framework import status
+from lcm.ns_pnfs.biz.create_pnf import CreatePnf
+from lcm.pub.utils import restcall
+
+
+class TestCreatePnfViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def tearDown(self):
+        pass
+
+    @mock.patch.object(restcall, 'call_req')
+    def test_do_biz(self, mock_call_req):
+        mock_call_req.return_value = [0, json.JSONEncoder().encode({
+            'id': str(uuid.uuid4()),
+            'pnfdId': 'test',
+            'pnfdName': 'test',
+            'pnfdVersion': 'v1.1.0',
+            'pnfdProvider': 'ZTE',
+            'pnfdInvariantId': str(uuid.uuid4())
+        }), '200']
+        id = str(uuid.uuid4())
+        data = {
+            "pnfId": id,
+            "pnfName": "Test PNF",
+            "pnfdId": str(uuid.uuid4()),
+            "pnfdInfoId": str(uuid.uuid4()),
+            "pnfProfileId": str(uuid.uuid4()),
+            "cpInfo": [
+                {
+                    "cpInstanceId": str(uuid.uuid4()),
+                    "cpdId": "pnf_ext_cp01",
+                    "cpProtocolData": []
+                }
+            ],
+            "emsId": str(uuid.uuid4()),
+            "nsInstances": str(uuid.uuid4()) + "," + str(uuid.uuid4())
+        }
+        ret = CreatePnf(data).do_biz()
+        self.assertIsNotNone(ret)
+
+    @mock.patch.object(restcall, 'call_req')
+    def test_rest_api(self, mock_call_req):
+        mock_call_req.return_value = [0, json.JSONEncoder().encode({
+            'id': str(uuid.uuid4()),
+            'pnfdId': 'test',
+            'pnfdName': 'test',
+            'pnfdVersion': 'v1.1.0',
+            'pnfdProvider': 'ZTE',
+            'pnfdInvariantId': str(uuid.uuid4())
+        }), '200']
+        id = str(uuid.uuid4())
+        data = {
+            "pnfId": id,
+            "pnfName": "Test PNF",
+            "pnfdId": str(uuid.uuid4()),
+            "pnfdInfoId": str(uuid.uuid4()),
+            "pnfProfileId": str(uuid.uuid4()),
+            "cpInfo": [
+                {
+                    "cpInstanceId": str(uuid.uuid4()),
+                    "cpdId": "pnf_ext_cp01",
+                    "cpProtocolData": []
+                }
+            ],
+            "emsId": str(uuid.uuid4()),
+            "nsInstances": str(uuid.uuid4()) + "," + str(uuid.uuid4())
+        }
+
+        response = self.client.post("/api/nslcm/v1/pnfs", data=data, format='json')
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
--- lcm/pub/database/models.py
+++ lcm/pub/database/models.py
@@ -330,3 +330,32 @@ class OOFDataModel(models.Model):
     cloud_owner = models.CharField(db_column='CLOUDOWNER', max_length=100, null=True, blank=True)
     cloud_region_id = models.CharField(db_column='CLOUDREGIONID', max_length=255, null=True, blank=True)
     vdu_info = models.TextField(db_column='VDUINFO', max_length=65535, null=True, blank=True)
+
+
+class PNFInstModel(models.Model):
+    class Meta:
+        db_table = 'NFVO_PNFINST'
+
+    pnfId = models.CharField(db_column='PNFID', primary_key=True, max_length=255)
+    pnfName = models.CharField(db_column='PNFNAME', max_length=255)
+    pnfdId = models.CharField(db_column='PNFDID', max_length=50)
+    pnfdInfoId = models.CharField(db_column='PNFDINFOID', max_length=100)
+    pnfProfileId = models.CharField(db_column='PNFPROFILEID', max_length=255)
+    cpInfo = models.TextField(db_column='CPINFO', max_length=255, null=True, blank=True)
+    emsId = models.CharField(db_column='EMSID', null=True, max_length=255)
+    nsInstances = models.TextField(db_column='NSINSTANCES', max_length=1000, null=True, blank=True)
+
+
+ 
+class SubscriptionModel(models.Model):
+    class Meta:
+        db_table = 'NFVO_SUBSCRIPTION'
+
+    subscription_id = models.CharField(db_column='SUBSCRIPTIONID', max_length=255, primary_key=True)
+    vnf_instance_filter = models.TextField(db_column='VNFINSTANCEFILTER', null=True)
+    notification_types = models.TextField(db_column='NOTIFICATIONTYPES', null=True)
+    operation_types = models.TextField(db_column='OPERATIONTYPES', null=True)
+    operation_states = models.TextField(db_column='OPERATIONSTATES', null=True)
+    callback_uri = models.CharField(db_column='CALLBACKURI', max_length=255)
+    links = models.TextField(db_column='LINKS', max_length=20000)
+
