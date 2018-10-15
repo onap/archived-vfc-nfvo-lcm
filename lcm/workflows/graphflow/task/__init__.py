2ed9e555ceef54bffd5e8e7fe1575a8e59643742
maopengzhang <zhang.maopeng1@zte.com.cn>
2018-10-15 15:34:36 +0800

    add sync task in workflow
    add sync task in workflow to support NS
    
    Change-Id: I89e6a26551d47218714526c677d551fc55fe1922
    Issue-ID: VFC-1041
    Signed-off-by: maopengzhang <zhang.maopeng1@zte.com.cn>

---
+++ lcm/workflows/graphflow/task/__init__.py
@@ -0,0 +1,19 @@
+# Copyright 2018 ZTE Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# from lcm.workflows.graphflow.task.async_rest_task import ASyncRestTask
+# from lcm.workflows.graphflow.task.async_task import AsyncTask
+# from lcm.workflows.graphflow.task.sync_rest_task import SyncRestTask
+# from lcm.workflows.graphflow.task.sync_task import SyncTask
+# from lcm.workflows.graphflow.task.task import Task
