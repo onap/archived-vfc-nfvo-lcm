3e957f8ba6c070faa39a733f3d548b8405d8bcef
Ruoyu Ying <ruoyu.ying@intel.com>
2018-11-05 16:58:21 +0800

    Comply with current data model in grant vnf
    Change the code to comply with current data model
    
    Change-Id: I3047fd240313eb0a48e2c69fa52a9ac1f6c395a3
    Issue-ID: VFC-1085
    Signed-off-by: Ruoyu Ying <ruoyu.ying@intel.com>

--- lcm/ns_vnfs/biz/grant_vnf.py
+++ lcm/ns_vnfs/biz/grant_vnf.py
@@ -105,12 +105,13 @@ class GrantVnf(object):
 
         offs = OOFDataModel.objects.filter(service_resource_id=ignore_case_get(self.data, "vnfInstanceId"))
         if offs.exists():
-            for off in offs:
+            vdu_info = eval(offs.vdu_info)
+            for vdu in vdu_info:
                 grant_resp['vimAssets']['computeResourceFlavours'].append({
-                    'vimConnectionId': off.vim_id,
-                    'resourceProviderId': off.vdu_name,
+                    'vimConnectionId': offs.vim_id,
+                    'resourceProviderId': vdu.get("vduName"),
                     'vnfdVirtualComputeDescId': None,  # TODO: required
-                    'vimFlavourId': off.flavor_name
+                    'vimFlavourId': vdu.get("flavorName")
                 })
                 # grant_resp['additionalparams'][off.vim_id] = off.directive
 
