98e74a1ff273e4843968894525e661a977c5052f
yangyan <yangyanyj@chinamobile.com>
2019-02-01 15:42:06 +0800

    add sub api in lcm
    Change-Id: I85d57fa3287b39c977a11837b290ef8b1590efa8
    Signed-off-by: yangyan <yangyanyj@chinamobile.com>
    Issue-ID: VFC-1253

--- lcm/pub/database/models.py
+++ lcm/pub/database/models.py
@@ -338,11 +338,16 @@ class SubscriptionModel(models.Model):
 
     subscription_id = models.CharField(db_column='SUBSCRIPTIONID', max_length=255, primary_key=True)
     vnf_instance_filter = models.TextField(db_column='VNFINSTANCEFILTER', null=True)
+    ns_instance_filter = models.TextField(db_column='NSINSTANCEFILTER', null=True)
     notification_types = models.TextField(db_column='NOTIFICATIONTYPES', null=True)
     operation_types = models.TextField(db_column='OPERATIONTYPES', null=True)
     operation_states = models.TextField(db_column='OPERATIONSTATES', null=True)
+    ns_component_types = models.TextField(db_column='NSCOMPONENTTYPES', null=True)
+    lcm_opname_impacting_nscomponent = models.TextField(db_column='LCMOPNAMEIMPACTINGNSCOMPONENT', null=True)
+    lcm_opoccstatus_impacting_nscomponent = models.TextField(db_column='LCMOPOCCSTATUSIMPACTINGNSCOMPONENT', null=True)
     callback_uri = models.CharField(db_column='CALLBACKURI', max_length=255)
     links = models.TextField(db_column='LINKS', max_length=20000)
+    auth_info = models.TextField(db_column='AUTHINFO', max_length=20000, blank=True, null=True)
 
 
 class PNFInstModel(models.Model):
