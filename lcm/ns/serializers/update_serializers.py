802e1f038d81be1bcfd3ffe6aca41683fb8c2e1c
yangyan <yangyanyj@chinamobile.com>
2019-03-15 11:30:15 +0800

    fix the serializer bug of nslcm
    Change-Id: If2304b3564d14ee3dd9c519f61feb4dfee9f51b6
    Issue-ID: VFC-1270
    Signed-off-by: yangyan <yangyanyj@chinamobile.com>

--- lcm/ns/serializers/update_serializers.py
+++ lcm/ns/serializers/update_serializers.py
@@ -149,11 +149,39 @@ class OperateVnfDataSerializer(serializers.Serializer):
 
 
 class ModifyVnfInfoDataSerializer(serializers.Serializer):
-    vnfInstanceId = serializers.CharField(help_text="Identifier of the VNF instance", required=True)
-    vnfInstanceName = serializers.CharField(help_text="New value of the vnfInstanceName attribute in VnfInstance",
-                                            required=False, allow_null=True)
-    vnfInstanceDescription = serializers.CharField(help_text="New value of the vnfInstanceDescription attribute in"
-                                                             "VnfInstance", required=False, allow_null=True)
+    vnfInstanceId = serializers.UUIDField(
+        help_text="Identifier of the VNF instance."
+    )
+    vnfInstanceName = serializers.CharField(
+        help_text="New value of the 'vnfInstanceName' attribute in 'VnfInstance', or 'null' to remove the attribute.",
+        max_length=255,
+        required=False,
+        allow_null=True,
+        allow_blank=True)
+    vnfInstanceDescription = serializers.CharField(
+        help_text="If present, this attribute signals modifications of the 'vnfInstanceDescription' attribute in "
+                  "'VnfInstance'",
+        required=False,
+        allow_null=True,
+        allow_blank=True)
+    vnfPkgId = serializers.UUIDField(
+        help_text="New value of the 'vnfPkgId' attribute in 'VnfInstance' The value 'null' is not permitted.."
+    )
+    vnfConfigurableProperties = serializers.DictField(
+        help_text="Modifications to entries in the 'vnfConfigurableProperties' list, as defined below this Table.",
+        child=serializers.CharField(help_text="KeyValue Pairs", allow_blank=True),
+        required=False,
+        allow_null=True)
+    metaData = serializers.DictField(
+        help_text="If present, this attribute signals modifications of certain 'metadata' attribute in 'vnfInstance'.",
+        child=serializers.CharField(help_text="KeyValue Pairs", allow_blank=True),
+        required=False,
+        allow_null=True)
+    extensions = serializers.DictField(
+        help_text="If present,this attribute signals modifications of certain 'extensions' attribute in 'vnfInstance'.",
+        child=serializers.CharField(help_text="KeyValue Pairs", allow_blank=True),
+        required=False,
+        allow_null=True)
 
 
 class ChangeExtVnfConnectivityDataSerializer(serializers.Serializer):
