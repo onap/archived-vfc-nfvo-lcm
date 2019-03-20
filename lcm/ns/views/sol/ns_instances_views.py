ed65df78a2235dbb5b320655698091ae41e5d65f
maopengzhang <zhang.maopeng1@zte.com.cn>
2019-03-20 20:20:47 +0800

    add license
    add license
    
    Change-Id: Ic9ce9a57c0c3094290a55c15fa14dda52a9fc77d
    Issue-ID: VFC-1211
    Signed-off-by: maopengzhang <zhang.maopeng1@zte.com.cn>

---
+++ lcm/ns/views/sol/ns_instances_views.py
@@ -0,0 +1,51 @@
+# Copyright 2019 ZTE Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+logger = logging.getLogger(__name__)
+
+
+class NSInstancesView(APIView):
+    def get(self, request):
+        logger.debug(request.query_params)
+        # todo
+
+    def post(self, request):
+        logger.debug("Enter NSInstancesView::POST ns_instances %s", request.data)
+        # todo
+        return Response(data={}, status=status.HTTP_201_CREATED)
+
+
+class IndividualNsInstanceView(APIView):
+    def get(self, request, id):
+        logger.debug("Enter IndividualNsInstanceView::get ns(%s)", id)
+        # todo
+        return Response(data={}, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        request_body=None,
+        responses={
+            status.HTTP_204_NO_CONTENT: None
+        }
+    )
+    def delete(self, request, id):
+        logger.debug("Enter IndividualNsInstanceView::DELETE ns_instance(%s)", id)
+        # todo
+        return Response(data={}, status=status.HTTP_204_NO_CONTENT)
