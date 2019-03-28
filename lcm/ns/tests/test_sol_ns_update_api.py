7303562c58fb058c9226c3c541427c2f36f3ea2a
maopengzhang <zhang.maopeng1@zte.com.cn>
2019-03-28 09:18:18 +0800

    add NS scale test usecase
    add NS scale test usecase
    
    Change-Id: I4df819ae1e4cc3ee561516abdd2888ea8d1ddc32
    Issue-ID: VFC-1215
    Signed-off-by: maopengzhang <zhang.maopeng1@zte.com.cn>

---
+++ lcm/ns/tests/test_sol_ns_update_api.py
@@ -0,0 +1,36 @@
+# Copyright 2019 ZTE Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from django.test import TestCase
+from rest_framework import status
+from lcm.pub.database.models import NSInstModel, JobModel
+
+
+class TestUpdateNsApi(TestCase):
+    def setUp(self):
+        self.url = "/api/nslcm/v1/ns_instances/%s/update"
+
+    def tearDown(self):
+        NSInstModel.objects.filter().delete()
+        JobModel.objects.filter().delete()
+
+    def test_method_not_allowed(self):
+        response = self.client.put(self.url % '1', data={}, format='json')
+        self.failUnlessEqual(status.HTTP_405_METHOD_NOT_ALLOWED, response.status_code)
+        response = self.client.patch(self.url % '1', data={}, format='json')
+        self.failUnlessEqual(status.HTTP_405_METHOD_NOT_ALLOWED, response.status_code)
+        response = self.client.delete(self.url % '1', data={}, format='json')
+        self.failUnlessEqual(status.HTTP_405_METHOD_NOT_ALLOWED, response.status_code)
+        response = self.client.get(self.url % '1', data={}, format='json')
+        self.failUnlessEqual(status.HTTP_405_METHOD_NOT_ALLOWED, response.status_code)
