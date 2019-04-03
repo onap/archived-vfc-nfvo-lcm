cfca2d69c507d002abb5bd134c1bb45d07fd69f4
yangyan <yangyanyj@chinamobile.com>
2019-04-03 10:55:35 +0800

    fix database bug
    Change-Id: Id4c1149ee3d1d348abe02cd92324499e4158201b
    Issue-ID: VFC-1334
    Signed-off-by: yangyan <yangyanyj@chinamobile.com>

--- docker/instance_init.sh
+++ docker/instance_init.sh
@@ -26,9 +26,9 @@ function migrate_database {
 }
 
 
-cd /service
-if [ ! -f dbexist.txt ]; then
-    echo 1 > dbexist.txt
+GET_NSLCM_DB="show databases like 'vfcnfvolcm';"
+VNFM_DB=`mysql -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -P$MYSQL_PORT -h$MYSQL_IP -e "GET_NSLCM_DB"`
+if [[ "$VNFM_DB" == "" ]]; then
     create_database
     migrate_database
 fi
