a6a72148279084d20d9e1e48444222de46b98260
fujinhua <fu.jinhua@zte.com.cn>
2019-04-04 09:46:37 +0800

    Update unit test for swagger
    Change-Id: I0265e759153fe1cc123ccd30c763059ca44260e4
    Issue-ID: VFC-1306
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- lcm/swagger/tests.py
+++ lcm/swagger/tests.py
@@ -1,29 +1,31 @@
-# Copyright 2016-2017 ZTE Corporation.
+# Copyright (C) 2019 ZTE. All Rights Reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at:
-#       http://www.apache.org/licenses/LICENSE-2.0
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# import json
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from django.test import TestCase
+from django.test import Client
+from rest_framework import status
 
-from rest_framework.test import APIClient
-# from rest_framework import status
 
-
-class SwaggerViewTest(TestCase):
+class TestSwagger(TestCase):
     def setUp(self):
-        self.client = APIClient()
+        self.client = Client()
 
     def tearDown(self):
         pass
 
-    # def test_swagger(self):
-    #  response = self.client.get("/api/nslcm/v1/swagger.json")
-    #  self.assertEqual(status.HTTP_200_OK, response.status_code, response.content)
-    #  with open('vfc.json', 'w') as swagger_file:
-    #      swagger_file.write(json.dumps(response.data))
+    def test_swagger_json(self):
+        url = "/api/nslcm/v1/swagger.json"
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual("2.0", response.data.get("swagger"))
