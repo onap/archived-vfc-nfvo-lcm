2dd726212aa85dbce717d3b1b1c0753cfa365406
yangyan <yangyanyj@chinamobile.com>
2019-04-18 10:18:03 +0800

    Optimized docker file code
    Change-Id: Id2d67853d1ab9e741952ca2844abaa177aa41cd8
    Signed-off-by: yangyan <yangyanyj@chinamobile.com>
    Issue-ID: VFC-1361

---
+++ docker/docker-env-conf.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+install_sf(){
+
+    apk --no-cache update
+    apk --no-cache add bash curl gcc wget mysql-client openssl-dev
+    apk --no-cache add python-dev libffi-dev musl-dev py2-virtualenv
+    wget -q -O vfc-nfvo-lcm.zip 'https://nexus.onap.org/service/local/artifact/maven/redirect?r=snapshots&g=org.onap.vfc.nfvo.lcm&a=vfc-nfvo-lcm&v=LATEST&e=zip' && \
+    unzip vfc-nfvo-lcm.zip && \
+    rm -rf vfc-nfvo-lcm.zip && \
+    pip install --upgrade setuptools pip && \
+    pip install --pre -r /service/vfc/nfvo/lcm/requirements.txt
+}
+
+add_user(){
+
+    apk --no-cache add sudo
+    addgroup -g 1000 -S onap && \
+    adduser onap -D -G onap -u 1000 && \
+	
+    chmod u+w /etc/sudoers && \
+    sed -i '/User privilege/a\\onap    ALL=(ALL:ALL) NOPASSWD:ALL' /etc/sudoers && \
+    chmod u-x /etc/sudoers && \
+    sudo chown onap:onap -R /service
+}
+
+config_logdir(){
+
+    if [ ! -d "/var/log/onap" ]; then
+       sudo mkdir /var/log/onap
+    fi 
+   
+    sudo chown onap:onap -R /var/log/onap
+    chmod g+s /var/log/onap
+    
+}
+
+clean_sf_cache(){
+
+    rm -rf /var/cache/apk/*
+    rm -rf /root/.cache/pip/*
+    rm -rf /tmp/*
+}
+
+install_sf
+wait
+add_user
+config_logdir
+clean_sf_cache
+
+
+
