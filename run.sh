4d3c50b33ac03e8d9c319d6654d92048cdb6a3ed
fujinhua <fu.jinhua@zte.com.cn>
2019-04-19 16:23:16 +0800

    Fix sh grammar error for nslcm
    Change-Id: I95c484d28ce30142fb68811c2c5f247047f64afe
    Issue-ID: VFC-1307
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- run.sh
+++ run.sh
@@ -21,7 +21,7 @@ fi
 # nohup python manage.py runserver 0.0.0.0:8403 > /dev/null &
 #nohup uwsgi --http :8403 -t 120 --module lcm.wsgi --master --processes 4 &
 
-if [ ${SSL_ENABLED} = "true" ]; then
+if [ "${SSL_ENABLED}" = "true" ]; then
     nohup uwsgi --https :8403,lcm/pub/ssl/cert/foobar.crt,lcm/pub/ssl/cert/foobar.key, HIGH -t 120 --module lcm.wsgi --master --processes 4 &
 else
     nohup uwsgi --http :8403 -t 120 --module lcm.wsgi --master --processes 4 &
