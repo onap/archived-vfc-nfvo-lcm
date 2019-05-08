21d02e6fd442b5a200661632db03e9297c970af4
yangyan <yangyanyj@chinamobile.com>
2019-05-08 14:43:23 +0800

    fix the bug of instance_config.sh
    Change-Id: If0eda4e0be8e751f99a4ab781b886fd551585a5d
    Issue-ID: VFC-1379
    Signed-off-by: yangyan <yangyanyj@chinamobile.com>

--- run.sh
+++ run.sh
@@ -22,9 +22,9 @@ fi
 #nohup uwsgi --http :8403 -t 120 --module lcm.wsgi --master --processes 4 &
 
 if [ "${SSL_ENABLED}" = "true" ]; then
-    nohup uwsgi --https :8403,lcm/pub/ssl/cert/foobar.crt,lcm/pub/ssl/cert/foobar.key, -t 120  --module lcm.wsgi --uid=onap --gid=onap --master --processes 4 &
+    nohup uwsgi --https :8403,lcm/pub/ssl/cert/foobar.crt,lcm/pub/ssl/cert/foobar.key, -t 120  --module lcm.wsgi --uid onap --gid onap --master --processes 4 &
 else
-    nohup uwsgi --http :8403 -t 120 --module lcm.wsgi --uid=onap --gid=onap --master --processes 4 &
+    nohup uwsgi --http :8403 -t 120 --module lcm.wsgi --uid onap --gid onap --master --processes 4 &
 fi
 
 
