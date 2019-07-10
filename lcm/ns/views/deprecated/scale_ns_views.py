c966dfcf121e2da2bfd9710a713ef7821311a905
fujinhua <fu.jinhua@zte.com.cn>
2019-07-10 10:57:31 +0800

    Refactor codes for manual scale ns
    Change-Id: Ib738fc6e9c5c7c4b4120402af6a1eebd2f15aad8
    Issue-ID: VFC-1429
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- lcm/ns/views/deprecated/scale_ns_views.py
+++ lcm/ns/views/deprecated/scale_ns_views.py
@@ -24,6 +24,7 @@ from lcm.ns.serializers.deprecated.ns_serializers import _ManualScaleNsReqSerial
 from lcm.pub.exceptions import NSLCMException
 from lcm.pub.utils.jobutil import JobUtil
 from lcm.jobs.enum import JOB_TYPE, JOB_ACTION
+from .common import view_safe_call_with_log
 
 logger = logging.getLogger(__name__)
 
@@ -36,27 +37,29 @@ class NSManualScaleView(APIView):
             status.HTTP_500_INTERNAL_SERVER_ERROR: "Inner error"
         }
     )
+    @view_safe_call_with_log(logger=logger)
     def post(self, request, ns_instance_id):
         logger.debug("Enter NSManualScaleView::post %s, %s", request.data, ns_instance_id)
-        job_id = JobUtil.create_job(JOB_TYPE.NS, JOB_ACTION.MANUAL_SCALE, ns_instance_id)
-        try:
-            req_serializer = _ManualScaleNsReqSerializer(data=request.data)
-            if not req_serializer.is_valid():
-                raise NSLCMException(req_serializer.errors)
-            req = request.data
-            scale_data = {}
-            scale_data['scaleType'] = req['scaleType']
-            scaleNsData = req['scaleNsData'][0]
-            scale_data['scaleNsData'] = {"scaleNsByStepsData": scaleNsData['scaleNsByStepsData'][0]}
-            NSManualScaleService(ns_instance_id, scale_data, job_id).start()
+        req_serializer = _ManualScaleNsReqSerializer(data=request.data)
+        if not req_serializer.is_valid():
+            raise NSLCMException(req_serializer.errors)
 
-            resp_serializer = _NsOperateJobSerializer(data={'jobId': job_id})
-            if not resp_serializer.is_valid():
-                raise NSLCMException(resp_serializer.errors)
+        req = request.data
+        scale_data = {}
+        scale_data['scaleType'] = req['scaleType']
+        scaleNsData = req['scaleNsData'][0]
+        scale_data['scaleNsData'] = {
+            "scaleNsByStepsData": scaleNsData['scaleNsByStepsData'][0]
+        }
+        job_id = JobUtil.create_job(
+            JOB_TYPE.NS,
+            JOB_ACTION.MANUAL_SCALE,
+            ns_instance_id
+        )
+        NSManualScaleService(ns_instance_id, scale_data, job_id).start()
 
-            return Response(data=resp_serializer.data, status=status.HTTP_202_ACCEPTED)
-        except Exception as e:
-            logger.error(traceback.format_exc())
-            JobUtil.add_job_status(job_id, 255, 'NS scale failed: %s' % e.args[0])
-            return Response(data={'error': 'NS scale failed: %s' % ns_instance_id},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        resp_serializer = _NsOperateJobSerializer(data={'jobId': job_id})
+        if not resp_serializer.is_valid():
+            raise NSLCMException(resp_serializer.errors)
+
+        return Response(data=resp_serializer.data, status=status.HTTP_202_ACCEPTED)
