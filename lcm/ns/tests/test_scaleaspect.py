9a9436acf183a6c1689fd6121e91a78c8cc04705
erlei <zhu.erlei@zte.com.cn>
2019-07-25 17:33:15 +0800

    ADD UT for ns_lcm Issue-ID: VFC-1429 Signed-off-by: zhuerlei <zhu.erlei@zte.com.cn>
    Issue-ID: VFC-1429
    Change-Id: I0bb67ab89b15ef5f69809d7055082c13551d53cc
    Signed-off-by: erlei <zhu.erlei@zte.com.cn>

--- lcm/ns/tests/test_scaleaspect.py
+++ lcm/ns/tests/test_scaleaspect.py
@@ -1,200 +1,122 @@
 # Copyright 2016-2018 ZTE Corporation.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #         http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
+import mock
+
 from django.test import TestCase
-from lcm.pub.database.models import NSInstModel
-from lcm.pub.database.models import NfInstModel
-from lcm.pub.utils.timeutil import now_time
-from lcm.ns.tests import VNFD_MODEL_DICT
+from lcm.ns.biz.scaleaspect import *
 
 
 class TestScaleAspect(TestCase):
 
     def setUp(self):
-        self.initInstModel()
-
-        self.init_scale_ns_data()
-
-        self.vnf_scale_info_list = [
-            {
-                "vnfd_id": "nf_zte_cscf",
-                "vnf_scaleAspectId": "mpu",
-                "numberOfSteps": "1"
-            },
-            {
-                "vnfd_id": "nf_zte_hss",
-                "vnf_scaleAspectId": "gsu",
-                "numberOfSteps": "2"
-            }
-        ]
-
-    def init_scale_ns_data(self):
-        self.ns_scale_aspect = "TIC_EDGE_IMS"
-        self.ns_scale_steps = "1"
-        self.ns_scale_direction = "SCALE_IN"
-        self.scaleNsData = [{
-            "scaleNsByStepsData": [
-                {
-                    "aspectId": self.ns_scale_aspect,
-                    "numberOfSteps": self.ns_scale_steps,
-                    "scalingDirection": self.ns_scale_direction
-                }
-            ]
-        }]
-
-        self.ns_scale_aspect2 = "TIC_EDGE_HW"
-        self.ns_scale_steps2 = "4"
-        self.scaleNsData2 = [{
-            "scaleNsByStepsData": [
-                {
-                    "aspectId": self.ns_scale_aspect2,
-                    "numberOfSteps": self.ns_scale_steps2,
-                    "scalingDirection": self.ns_scale_direction
-                }
-            ]
-        }]
-
-    def initInstModel(self):
-        self.nsd_id = "23"
-        self.ns_inst_id = "1"
-        self.ns_name = "ns_1"
-        self.ns_package_id = "ns_zte"
-        self.description = "ns_zte"
-        self.global_customer_id = "global_customer_id"
-        self.service_type = "service_role"
-
-        NSInstModel(id=self.ns_inst_id,
-                    name=self.ns_name,
-                    nspackage_id=self.ns_package_id,
-                    nsd_id=self.nsd_id,
-                    description=self.description,
-                    status='empty',
-                    lastuptime=now_time(),
-                    global_customer_id=self.global_customer_id,
-                    service_type=self.service_type).save()
-
-        self.nf_inst_id = "231"
-        self.ns_inst_id = "1"
-        self.nf_name = "name_1"
-        self.vnf_id = "1"
-        self.vnfm_inst_id = "1"
-        self.package_id = "nf_zte_cscf"
-        self.nf_uuid = "abc34-345a-de13-ab85-ijs9"
-
-        NfInstModel.objects.create(
-            nfinstid=self.nf_inst_id,
-            nf_name=self.nf_name,
-            vnf_id=self.vnf_id,
-            vnfm_inst_id=self.vnfm_inst_id,
-            ns_inst_id=self.ns_inst_id,
-            max_cpu='14',
-            max_ram='12296',
-            max_hd='101',
-            max_shd="20",
-            max_net=10,
-            status='active',
-            mnfinstid=self.nf_uuid,
-            package_id=self.package_id,
-            vnfd_model=json.dumps(VNFD_MODEL_DICT))
-
-        # Create a second vnf instance
-        self.nf_inst_id = "232"
-        self.package_id = "nf_zte_hss"
-        self.nf_uuid = "abc34-3g5a-de13-ab85-ijs3"
-
-        NfInstModel.objects.create(
-            nfinstid=self.nf_inst_id,
-            nf_name=self.nf_name,
-            vnf_id=self.vnf_id,
-            vnfm_inst_id=self.vnfm_inst_id,
-            ns_inst_id=self.ns_inst_id,
-            max_cpu='14',
-            max_ram='12296',
-            max_hd='101',
-            max_shd="20",
-            max_net=10,
-            status='active',
-            mnfinstid=self.nf_uuid,
-            package_id=self.package_id,
-            vnfd_model=json.dumps(VNFD_MODEL_DICT))
-
-    def add_another_nf_instance(self):
-        # Create a third vnf instance
-        nf_inst_id = "233"
-        package_id = "nf_zte_hss"
-        nf_uuid = "ab34-3g5j-de13-ab85-ij93"
-
-        NfInstModel.objects.create(
-            nfinstid=nf_inst_id,
-            nf_name=self.nf_name,
-            vnf_id=self.vnf_id,
-            vnfm_inst_id=self.vnfm_inst_id,
-            ns_inst_id=self.ns_inst_id,
-            max_cpu='14',
-            max_ram='12296',
-            max_hd='101',
-            max_shd="20",
-            max_net=10,
-            status='active',
-            mnfinstid=nf_uuid,
-            package_id=package_id,
-            vnfd_model=json.dumps(VNFD_MODEL_DICT))
-
-    def add_new_vnf_instance(self):
-        # Create a third vnf instance
-        nf_inst_id = "241"
-        package_id = "nf_hw_cscf"
-        nf_uuid = "ab34-3g5j-de13-aa85-ij93"
-
-        NfInstModel.objects.create(
-            nfinstid=nf_inst_id,
-            nf_name=self.nf_name,
-            vnf_id=self.vnf_id,
-            vnfm_inst_id=self.vnfm_inst_id,
-            ns_inst_id=self.ns_inst_id,
-            max_cpu='14',
-            max_ram='12296',
-            max_hd='101',
-            max_shd="20",
-            max_net=10,
-            status='active',
-            mnfinstid=nf_uuid,
-            package_id=package_id,
-            vnfd_model=json.dumps(VNFD_MODEL_DICT))
-
-        # Create a third vnf instance
-        nf_inst_id = "242"
-        package_id = "nf_hw_hss"
-        nf_uuid = "ab34-3g5j-de13-aa85-id93"
-
-        NfInstModel.objects.create(
-            nfinstid=nf_inst_id,
-            nf_name=self.nf_name,
-            vnf_id=self.vnf_id,
-            vnfm_inst_id=self.vnfm_inst_id,
-            ns_inst_id=self.ns_inst_id,
-            max_cpu='14',
-            max_ram='12296',
-            max_hd='101',
-            max_shd="20",
-            max_net=10,
-            status='active',
-            mnfinstid=nf_uuid,
-            package_id=package_id,
-            vnfd_model=json.dumps(VNFD_MODEL_DICT))
+        pass
 
     def tearDown(self):
         NSInstModel().clean()
         NfInstModel().clean()
+
+    def test_mapping_conv(self):
+        keyword_map = {'a': 1, 'b': 2, 'c': 3, 'd': {'d.1': 'd.1'}}
+        rest_return = {'b': 2, 'C': 3}
+        resp_data = mapping_conv(keyword_map, rest_return)
+        self.assertEqual(resp_data, {'a': '', 'b': 2, 'c': 3, 'd': {'d.1': ''}})
+
+    def test_get_vnf_instance_id_list_when_no_model(self):
+        self.assertEqual(get_vnf_instance_id_list('package_1'), [])
+
+    def test_check_scale_list_when_empty_vnf_scale_list(self):
+        try:
+            check_scale_list([], 'ns_instanceId_01', '', '')
+        except Exception as e:
+            self.assertEqual(e.args, ('The scaling option[ns=ns_instanceId_01, aspect=, step=] does not exist. '
+                                      'Pls check the config file.',))
+
+    @mock.patch.object(os.path, 'abspath')
+    def test_get_scale_vnf_data(self, mock_abspath):
+        mock_abspath.return_value = 'lcm/1/2/3.py'
+        scale_ns_data = {'scaleNsByStepsData': {'aspectId': 'TIC_EDGE_IMS', 'numberOfSteps': 1,
+                                                'scalingDirection': 'r'}}
+        ns_instance_id = 'ns_instanceId_01'
+        scale_vnf_data_list = get_scale_vnf_data(scale_ns_data, ns_instance_id)
+        success_list = [
+            {
+                'vnfInstanceId': 'nf_zte_cscf',
+                'scaleByStepData': {
+                    'type': 'r',
+                    'aspectId': 'gsu',
+                    'numberOfSteps': '1'
+                }
+            },
+            {
+                'vnfInstanceId': 'nf_zte_hss',
+                'scaleByStepData': {
+                    'type': 'r',
+                    'aspectId': 'gpu',
+                    'numberOfSteps': '3'
+                }
+            }
+        ]
+        self.assertEqual(scale_vnf_data_list, success_list)
+
+    def test_get_nsd_id_when_none(self):
+        self.assertEqual(get_nsdId('ns_instance_id_01'), None)
+
+    def test_check_and_set_params_when_scale_ns_data_is_none(self):
+        try:
+            check_and_set_params(None, '')
+        except Exception as e:
+            self.assertEqual(e.args, ('Error! scaleNsData in the request is Empty!',))
+
+    def test_check_and_set_params_when_scale_ns_by_steps_data_is_none(self):
+        scale_ns_data = {'scaleNsByStepsData': None}
+        try:
+            check_and_set_params(scale_ns_data, '')
+        except Exception as e:
+            self.assertEqual(e.args, ('Error! scaleNsByStepsData in the request is Empty!',))
+
+    @mock.patch.object(os.path, 'abspath')
+    def test_get_scale_vnf_data_info_list(self, mock_abspath):
+        NSInstModel(id='ns_instanceId_02', nsd_id='02').save()
+        NfInstModel(package_id='nf_hw_cscf', status=VNF_STATUS.ACTIVE, nfinstid='nfinst_01').save()
+        mock_abspath.return_value = 'lcm/1/2/3.py'
+        scale_ns_data = {
+            'scaleNsByStepsData': {'aspectId': 'TIC_EDGE_HW', 'numberOfSteps': 4, 'scalingDirection': 'r'}}
+        ns_instance_id = 'ns_instanceId_02'
+        scale_vnf_data_info_list = get_scale_vnf_data_info_list(scale_ns_data, ns_instance_id)
+        success_list = [
+            {
+                'vnfInstanceId': 'nfinst_01',
+                'scaleByStepData': {
+                    'type': 'r',
+                    'aspectId': 'gsu',
+                    'numberOfSteps': '1'
+                }
+            }
+        ]
+        self.assertEqual(scale_vnf_data_info_list, success_list)
+
+    def test_get_vnf_scale_info_when_return_none(self):
+        filename = 'lcm/ns/tests/data/scalemapping.json'
+        ns_instance_id = 'ns_instanceId_03'
+        self.assertEqual(get_vnf_scale_info(filename, ns_instance_id, '', ''), None)
+
+    def test_get_scale_vnf_data_from_json_when_return_none(self):
+        self.assertEqual(get_scale_vnf_data_from_json({}, '', '', ''), None)
+
+
+
+
+
+
