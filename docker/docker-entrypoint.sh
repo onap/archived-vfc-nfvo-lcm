a6b981f2d00fcef2fecf855f3957f5798f3f0805
yangyan <yangyanyj@chinamobile.com>
2020-02-13 14:21:23 +0800

    Fix the problem of registering MSB with HTTPS
    Change-Id: I7da5e634c3b92c8d261f3791d22eacb0036d2ac2
    Issue-ID: VFC-1621
    Signed-off-by: yangyan <yangyanyj@chinamobile.com>

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -23,7 +23,11 @@ echo "MYSQL_ADDR=$MYSQL_ADDR"
 # Wait for MSB initialization
 echo "Wait for MSB initialization"
 for i in {1..5}; do
-    curl -sS -m 1 $MSB_ADDR > /dev/null && break
+    curl -sS -m 1 $MSB_PROTO:$MSB_ADDR/msb -k > /dev/null
+    res=$i?
+    if [ $res -ne 0 ]; then
+        break
+    fi
     sleep $i
 done
 
