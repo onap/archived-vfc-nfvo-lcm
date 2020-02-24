93b5aa6534dfe8beaf789dc08e70edc85982555d
yangyan <yangyanyj@chinamobile.com>
2020-02-24 14:24:37 +0800

    Modify test scripts of F version and add instructions about it
    Change-Id: Ic0fb0effc701c47ee5609a5cb88d25619ce05c69
    Issue-ID: VFC-1621
    Signed-off-by: yangyan <yangyanyj@chinamobile.com>

---
+++ resources/testscripts/F-version/vnf/vnf_download.py
@@ -0,0 +1,10 @@
+import requests
+import sys
+
+requests.packages.urllib3.disable_warnings()
+id = sys.argv[1]
+url = 'https://192.168.235.89:30283/api/vnfpkgm/v1/vnf_packages/' + id + '/package_content'
+resp = requests.get(url, verify=False)
+local_file = open(r'./name.csar', 'wb')
+local_file.write(resp.content)
+local_file.close()
