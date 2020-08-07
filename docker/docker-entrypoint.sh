7250a283491694e08aa9e9cfb283e59b62833721
dyh <dengyuanhong@chinamobile.com>
2020-08-07 11:30:10 +0800

    Don't modify the config file with sed in the docker entrypoint script
    Issue-ID: VFC-1623
    Change-Id: I5fe44e016bfa451bc257d51e634d302c11641360
    Signed-off-by: dyh <dengyuanhong@chinamobile.com>

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,42 +1,8 @@
 #!/bin/bash
 
-if [ -z "$SERVICE_IP" ]; then
-    export SERVICE_IP=`hostname -i`
-fi
-echo "SERVICE_IP=$SERVICE_IP"
-
-if [ -z "$MSB_ADDR" ]; then
-    echo "Missing required variable MSB_ADDR: Microservices Service Bus address <ip>:<port>"
-    exit 1
-fi
-echo "MSB_ADDR=$MSB_ADDR"
-
-if [ -z "$MYSQL_ADDR" ]; then
-    echo "Missing required variable MYSQL_ADDR: <ip>:<port>"
-    exit 1
-fi
-echo "MYSQL_ADDR=$MYSQL_ADDR"
-
-# Wait for MSB initialization
-echo "Wait for MSB initialization"
-for i in {1..5}; do
-    curl -sS -m 1 $MSB_PROTO://$MSB_ADDR/msb -k > /dev/null
-    res=$?
-    if [ $res -ne 0 ]; then
-        break
-    fi
-    sleep $i
-done
-
-# Wait for DB initialization
-echo "Wait for DB initialization"
-for i in {1..5}; do
-    mysql -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -h`echo $MYSQL_ADDR | cut -d: -f 1` -P`echo $MYSQL_ADDR | cut -d: -f 2` -e "show databases;" > /dev/null && echo "DB initialization completed" && break
-    sleep $i && echo "`echo $MYSQL_ADDR | cut -d: -f 1` connection failed"
-done
-
-# Configure service based on docker environment variables
-vfc/nfvo/lcm/docker/instance_config.sh
+# Configure config file based on environment variables
+python vfc/nfvo/lcm/lcm/pub/config/config.py
+cat vfc/nfvo/lcm/lcm/pub/config/config.py
 
 # microservice-specific one-time initialization
 vfc/nfvo/lcm/docker/instance_init.sh
