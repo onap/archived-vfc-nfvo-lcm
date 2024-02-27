533bb5d5cce1063795d4ddd79d473f9db0c9744f
thmsdt <thomas.kulik@telekom.de>
2024-02-27 11:13:12 +0100

    update link to upper-constraints.txt
    Issue-ID: DOC-823
    
    Change-Id: Ib7a0f7b5741088602e7485d1f790e3b3133c5f15
    Signed-off-by: thmsdt <thomas.kulik@telekom.de>

--- lcm/ns_vnfs/biz/create_vnfs.py
+++ lcm/ns_vnfs/biz/create_vnfs.py
@@ -259,7 +259,7 @@ class CreateVnfs(Thread):
             # identifiers.append(vim_info[1])
             # cloudOwner = vim_info[0]
             identifiers = list()
-            if type(self.vim_id) == str:
+            if isinstance(self.vim_id, str):
                 self.vim_id = json.loads(self.vim_id)
             identifiers.append(self.vim_id['cloud_regionid'])
             cloudOwner = self.vim_id['cloud_owner']
